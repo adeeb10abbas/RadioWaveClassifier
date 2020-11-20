acdc8774513dc47a3ce6128e88243ca968c60054
adeeb10abbas <adeeb.abbas@drexel.edu>
2020-11-20 18:19:50 -0500

    cleanup

--- sample.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-# tr_labels = np.load("/home/arrowhead/dataset_2018/train/labels.npy")
-# tr_signals = np.load("/home/arrowhead/dataset_2018/train/signals.npy")
-tr_snrs = np.load("/home/arrowhead/dataset_2018/train/snrs.npy")
-
-print(type(tr_snrs[1]))
